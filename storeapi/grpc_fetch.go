716bc97455f4183f336d274e4e2a19a320be88e2
Daniil Porokhnin <dkharmsd@gmail.com>
2025-08-01 16:06:24 +0300

    feat: implement offloading in S3 storage

--- storeapi/grpc_fetch.go
+++ storeapi/grpc_fetch.go
@@ -12,6 +12,7 @@ import (
 	"go.opencensus.io/trace"
 	"go.uber.org/zap"
 
+	"github.com/ozontech/seq-db/fracmanager"
 	"github.com/ozontech/seq-db/logger"
 	"github.com/ozontech/seq-db/metric"
 	"github.com/ozontech/seq-db/pkg/storeapi"
@@ -68,7 +69,7 @@ func (g *GrpcV1) doFetch(ctx context.Context, req *storeapi.FetchRequest, stream
 	dp := acquireDocFieldsFilter(req.FieldsFilter)
 	defer releaseDocFieldsFilter(dp)
 
-	docsStream := newDocsStream(ctx, ids, g.fetchData.docFetcher, g.fracManager.GetAllFracs())
+	docsStream := newDocsStream(ctx, ids, g.fetchData.docFetcher, g.fracManager.GetFracs(fracmanager.FracTypeLocal|fracmanager.FracTypeRemote))
 	for _, id := range ids {
 		workTime := time.Now()
 		doc, err := docsStream.Next()
