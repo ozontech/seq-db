70d5249d41271bcfc24c10a28d4f4e53e4cfd2f2
Andrei Cheboksarov <37665782+0xaa4eb@users.noreply.github.com>
2025-10-31 22:34:28 +0400

    dirty if in streaming_doc.go

--- proxy/search/streaming_doc.go
+++ proxy/search/streaming_doc.go
@@ -29,6 +29,11 @@ func NewStreamingDoc(idSource seq.IDSource, data []byte) StreamingDoc {
 
 func unpackDoc(data []byte, source uint64) StreamingDoc {
 	block := storage.DocBlock(data)
+	mid := block.GetExt1()
+	// micro to milli
+	if mid > 1000000000000000 {
+		mid = mid / 1000
+	}
 	doc := StreamingDoc{
 		ID: seq.ID{
 			MID: seq.MID(block.GetExt1()),
