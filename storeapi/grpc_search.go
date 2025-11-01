f70c8fe8a0455b9ea3d62ba131c6cdacceddff49
Andrei Cheboksarov <37665782+cheb0@users.noreply.github.com>
2025-11-01 19:11:10 +0400

    dbg print

--- storeapi/grpc_search.go
+++ storeapi/grpc_search.go
@@ -216,7 +216,13 @@ func (g *GrpcV1) doSearch(
 	// TODO remove dbg logg
 	resp := buildSearchResponse(qpr)
 	if len(qpr.IDs) > 0 {
-		logger.Info(fmt.Sprintf("responding with IDS: %d", qpr.IDs[0].ID.MID))
+		logger.Info(fmt.Sprintf("responding with IDS: %d %d", qpr.IDs[0].ID.MID, qpr.IDs[0].Source))
+	}
+	if len(qpr.Histogram) > 0 {
+		for k, v := range qpr.Histogram {
+			logger.Info(fmt.Sprintf("responding with hist: %d -> %d", k, v))
+			break
+		}
 	}
 
 	return resp, nil
