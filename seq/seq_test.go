c508bcb321fb479a1462f77a594d3b275edc9d78
Andrei Cheboksarov <37665782+cheb0@users.noreply.github.com>
2025-11-10 21:59:24 +0400

    fix MID->Time overflow

--- seq/seq_test.go
+++ seq/seq_test.go
@@ -1,7 +1,9 @@
 package seq
 
 import (
+	"math"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -9,4 +11,14 @@ import (
 func TestFromString(t *testing.T) {
 	_, err := FromString("abaf05877b010000-2402dc02d60615cc")
 	assert.NoError(t, err)
+}
+
+func TestTimeToMIDConversionOverflow(t *testing.T) {
+	timestamp := time.Now()
+	assert.EqualExportedValues(t, timestamp, MID(timestamp.UnixNano()).Time())
+
+	// check that we do not overflow on huge values
+	maxMID := MID(math.MaxUint64)
+	assert.Equal(t, 584556019, maxMID.Time().Year())
+	assert.Equal(t, 584556019, MIDToTime(maxMID).Year())
 }
