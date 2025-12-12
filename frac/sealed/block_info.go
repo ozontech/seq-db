a354d2b3a7745f0861d84505623b16568a3bdb33
Andrei Cheboksarov <37665782+cheb0@users.noreply.github.com>
2025-12-12 15:28:47 +0400

    Fix unneeded conversion

--- frac/sealed/block_info.go
+++ frac/sealed/block_info.go
@@ -6,10 +6,8 @@ import (
 
 	"go.uber.org/zap"
 
-	"github.com/ozontech/seq-db/config"
 	"github.com/ozontech/seq-db/frac/common"
 	"github.com/ozontech/seq-db/logger"
-	"github.com/ozontech/seq-db/seq"
 )
 
 const seqDBMagic = "SEQM"
@@ -40,11 +38,5 @@ func (b *BlockInfo) Unpack(data []byte) error {
 		return errors.New("stats unmarshaling error")
 	}
 	b.Info.MetaOnDisk = 0 // todo: make this correction on sealing and remove this next time
-
-	if b.Info.BinaryDataVer >= config.BinaryDataV2 {
-		b.Info.From = seq.NanosToMID(uint64(b.Info.From))
-		b.Info.To = seq.NanosToMID(uint64(b.Info.To))
-	}
-
 	return nil
 }
