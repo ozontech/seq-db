1cff88b6d7b302cbded424dd205c6c602946e116
Daniil Forshev <dforshev@ozon.ru>
2026-01-15 15:09:24 +0500

    fix: fix rebase conflicts

--- asyncsearcher/metrics.go
+++ asyncsearcher/metrics.go
@@ -10,7 +10,7 @@ var (
 		Namespace: "seq_db_store",
 		Subsystem: "async_search",
 		Name:      "in_progress",
-		Help:      "Amount of active async searches in progress",
+		Help:      "Number of active async searches in progress",
 	})
 	asyncSearchConcurrencyLimit = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "seq_db_store",
@@ -21,21 +21,25 @@ var (
 	asyncSearchDiskUsage = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "seq_db_store",
 		Subsystem: "async_search",
-		Name:      "disk_usage_bytes_total",
+		Name:      "disk_usage_bytes",
+		Help:      "Disk space used by async search files in bytes by file type",
 	}, []string{"file_type"})
 	asyncSearchDiskUsageLimit = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "seq_db_store",
 		Subsystem: "async_search",
 		Name:      "disk_usage_limit_bytes",
+		Help:      "Maximum disk space async searches is allowed to use in bytes",
 	})
 	asyncSearchStoredRequests = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "seq_db_store",
 		Subsystem: "async_search",
 		Name:      "stored_requests",
+		Help:      "Number of stored async search requests",
 	})
 	asyncSearchReadOnly = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "seq_db_store",
 		Subsystem: "async_search",
 		Name:      "read_only",
+		Help:      "Indicates if store is in async search read-only mode (can not start a new async search) due to disk usage limits",
 	})
 )
