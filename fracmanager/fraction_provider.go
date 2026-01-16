378c06ddf1f01ddba519c386e6e7a49f2793dc1d
Che <107068454+eguguchkin@users.noreply.github.com>
2026-01-16 18:45:59 +0300

    refactor(fracmanager): using fifo queues of fractions (#268)

--- fracmanager/fraction_provider.go
+++ fracmanager/fraction_provider.go
@@ -122,7 +122,7 @@ func (fp *fractionProvider) Seal(active *frac.Active) (*frac.Sealed, error) {
 
 // Offload uploads fraction to S3 storage and returns a remote fraction
 // IMPORTANT: context controls timeouts and operation cancellation
-func (fp *fractionProvider) Offload(ctx context.Context, f frac.Fraction) (*frac.Remote, error) {
+func (fp *fractionProvider) Offload(ctx context.Context, f *frac.Sealed) (*frac.Remote, error) {
 	mustBeOffloaded, err := f.Offload(ctx, s3.NewUploader(fp.s3cli))
 	if err != nil {
 		return nil, err
